9333e119cf9343951cfec3c65f7887c84a22a1f1
Joshua Westerheide <dev@jdoubleu.de>
2016-04-23 21:42:11 +0200

    Implemented autoloading in index

--- index.php
+++ index.php
@@ -5,3 +5,11 @@
  *
  * Will load all configuration and the main class.
  */
+
+// Load files
+require_once 'Classes/Logger.php';
+require_once 'Classes/GitLabCD.php';
+
+// Create instance of the main handler
+$handler = new GitLabCD();
+$handler->initialize();
