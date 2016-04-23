8b3e09dbed1cf4dc4d66cf09935a82d1a1468abc
Joshua Westerheide <dev@jdoubleu.de>
2016-04-23 20:47:14 +0200

    Added constructor to main class and a config field. Implemented parsing of config.json into config field.

--- Classes/GitLabCD.php
+++ Classes/GitLabCD.php
@@ -5,5 +5,15 @@
  * @author Joshua Westerheide
  */
 class GitLabCD {
+	
+	/** @var array $config Local config array. */
+	private $config = array();
 
+	/**
+	 * GitLabCD constructor.
+	 */
+	public function __construct() {
+		// Get configuration
+		$this->config = json_decode(file_get_contents(dirname(__FILE__) . "/../Config/config.json"));
+	}
 }
