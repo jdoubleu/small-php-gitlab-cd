13eeead07f7a0ab881dd41e01f0ed4cb45837175
Joshua Westerheide <dev@jdoubleu.de>
2016-04-23 21:36:15 +0200

    Create and store logger in main class

--- Classes/GitLabCD.php
+++ Classes/GitLabCD.php
@@ -9,11 +9,17 @@ class GitLabCD {
 	/** @var array $config Local config array. */
 	private $config = array();
 
+	/** @var Logger $logger instance of a logger */
+	private $logger = null;
+
 	/**
 	 * GitLabCD constructor.
 	 */
 	public function __construct() {
 		// Get configuration
 		$this->config = json_decode(file_get_contents(dirname(__FILE__) . "/../Config/config.json"));
+
+		// Get a logger
+		$this->logger = Logger::getInstance();
 	}
 }
