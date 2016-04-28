9c582bebdc4d228a765ceb9c4fe429777024cceb
Joshua Westerheide <dev@jdoubleu.de>
2016-04-28 18:39:51 +0200

    Added doc and intro in both script files deploy and deploy-config

---
+++ deploy-config.example.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Configuration file
+ *
+ * This file contains all options needed for deployment. This will be included in the deploy
+ * script. Copy this file and rename it to `deploy-config.php`.
+ *
+ * @version 0.2
+ */
---
+++ deploy.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Small PHP GitLab CD
+ *
+ * A small and simple php script to deploy artifacts generated with and by the GitLab CI (https://about.gitlab.com/gitlab-ci/).
+ * Have a look at the README file for more information.
+ *
+ * @version 0.2
+ * @license MIT
+ * @link https://github.com/jdoubleu/small-php-gitlab-cd
+ */
