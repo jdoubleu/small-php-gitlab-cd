b4ab3dbdf5f89928376952db4d5c72025a576f4c
Joshua Westerheide <dev@jdoubleu.de>
2016-04-28 19:48:01 +0200

    Added initial checks in deployment script

--- deploy.php
+++ deploy.php
@@ -9,3 +9,24 @@
  * @license MIT
  * @link https://github.com/jdoubleu/small-php-gitlab-cd
  */
+
+/**
+ * Checks if this script is called via web server or via CLI
+ */
+define("MODE", (!empty($argc) && isset($argc[0]) ? "CLI" : "REQUEST"));
+
+/**
+ * Looks for a config file named after this file.
+ * The config file must start with the name of this file without an extension
+ * (e.g. deploy-config.php for deploy.php, test-config.php for test.php, ...).
+ *
+ * It is recommended to use an external config file rather than changing default
+ * configuration in this file.
+ */
+if(file_exists($config_file = basename(__FILE__,'-php') . '-config.php')) {
+	define("CONFIG_FILE", $config_file);
+	require_once $config_file;
+} else {
+	define("CONFIG_FILE", __FILE__);
+}
+
