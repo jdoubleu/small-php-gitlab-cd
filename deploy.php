9593934bdb9e88d556efaf6ff055d13eac9d6f2d
Joshua Westerheide <dev@jdoubleu.de>
2016-05-05 16:46:51 +0200

    Replaced old script exiting method using error function with new one.

--- deploy.php
+++ deploy.php
@@ -112,7 +112,7 @@ if(isset($_SERVER['QUERY_STRING']))
 // Check if secret token is need and given
 if(MODE == "REQUEST") {
 	if(!isset($_REQUEST['secret_token']) || $_REQUEST['secret_token'] != $CONFIG['secret_token'])
-		log("Invalid secret token! Aborting") && error(100);
+		log("Invalid secret token! Aborting", 100);
 }
 
 /*
@@ -125,15 +125,15 @@ $neededBinaries = array('curl', 'unzip', 'rsync');
 foreach($neededBinaries as $bin) {
 	$path = trim(shell_exec('which ' . $bin));
 	if(!$path)
-		log("Binaries for " . $bin . " not found! Aborting") && error(200);
+		log("Binaries for " . $bin . " not found! Aborting", 200);
 }
 
 // Reads incoming payload or argument
 if(MODE == "REQUEST") {
 	if(($requestPayload = file_get_contents('php://input')) == false)
-		log("Unknown Request Payload") && error(111);
+		log("Unknown Request Payload", 111);
 	elseif(!$requestPayload = json_decode($requestPayload, true))
-		log("Request Payload couldn't be analyzed. Failed with json decode error: " . json_last_error_msg()) && error(112);
+		log("Request Payload couldn't be analyzed. Failed with json decode error: " . json_last_error_msg(), 112);
 	else
 		log("Got a request payload!");
 } elseif(MODE == "CLI") {
@@ -142,23 +142,23 @@ if(MODE == "REQUEST") {
 	elseif($pkey = array_search('-p', $argv) && isset($argv[$pkey+1]))
 		$project_id = $argv[$pkey+1];
 	else
-		log("No project id given! Use -p parameter (See help for more information).") && error(114);
+		log("No project id given! Use -p parameter (See help for more information).", 114);
 
 	if($bkey = array_search('-b', $argv) && isset($argv[$bkey+1]))
 		$build_id = $argv[$bkey+1];
 	else
-		log("A build id is not given! Use -b parameter (See help for more information).") && error(113);
+		log("A build id is not given! Use -b parameter (See help for more information).", 113);
 }
 
 // Check for project id in HTTP REQUEST
 if(MODE == "REQUEST" && $CONFIG['project_id'] >= 0 && $CONFIG['project_id'] != $requestPayload['project_id'])
-	log("Project id is not given!") && error(102);
+	log("Project id is not given!", 102);
 else
 	$project_id = $requestPayload['project_id'];
 
 // Check if correct object kind in payload if request
 if(MODE == "REQUEST" && $requestPayload['object_kind'] != "build")
-	log("Invalid object_kind in request payload. Expected \"build\" got \"" . $requestPayload['object_kind'] . "\"!") && error(122);
+	log("Invalid object_kind in request payload. Expected \"build\" got \"" . $requestPayload['object_kind'] . "\"!", 122);
 
 // Get build id
 if(MODE == "REQUEST")
@@ -166,7 +166,7 @@ if(MODE == "REQUEST")
 
 // Check build status
 if(MODE == "REQUEST" && $requestPayload['build_status'] != "success")
-	log("The build failed! Need a successful build!") && error(123);
+	log("The build failed! Need a successful build!", 123);
 
 // Check ref/branches
 if(MODE == "REQUEST" && !in_array($requestPayload['ref'], $CONFIG['branches']))
@@ -176,7 +176,7 @@ if(MODE == "REQUEST" && !in_array($requestPayload['ref'], $CONFIG['branches']))
  * Test TMP DIR
  */
 if(!file_exists($CONFIG['tmp_dir']) || !is_dir($CONFIG['tmp_dir']))
-	log('TMP dir does not exist or is not a directory! Aborting.') && error(250);
+	log('TMP dir does not exist or is not a directory! Aborting.', 250);
 
 /*
  * Set up vars for storing paths
@@ -239,12 +239,12 @@ foreach($commands as $cmd) {
 
 	// on error
 	if(!$code) {
-		log("Command execution ended with an error! Aborting.");
+		log("Command execution ended with an error!");
 
 		if(isset($commands['cleanup']))
 			log("Cleaning tmp dir. Executing '" . $commands['cleanup'] . "'") && shell_exec($commands['cleanup']);
 
-		error(301);
+		log("Aborting.", 301);
 	}
 }
 
